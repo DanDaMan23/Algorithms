ecdeb32961150c595eba57004ae28abc0c9e8635
DanDaMan23 <danlawrcons.gmail.com>
2021-01-13 23:42:34 -0600

    Add project files.

---
+++ Algorithms/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            int[] numbers = new int[10];
+            
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = rnd.Next(0, 30);
+            }
+
+            //foreach (int x in Sorting.SelectionSort(numbers))
+            //{
+            //    Console.WriteLine(x);
+            //}
+
+            Console.WriteLine();
+
+            foreach (int x in Sorting.InsertionSort(numbers))
+            {
+                Console.WriteLine(x);
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ Algorithms/Sorting.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class Sorting
+    {
+        public static int[] SelectionSort(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int minPos = i;
+                for (int j = i; j < numbers.Length; j++)
+                {
+                    if (numbers[j] < numbers[minPos])
+                    {
+                        minPos = j;
+                    }
+                }
+
+                int temp = numbers[i];
+                numbers[i] = numbers[minPos];
+                numbers[minPos] = temp;
+            }
+
+            return numbers;
+        }
+
+        public static int[] InsertionSort(int[] numbers)
+        {
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                int j = i;
+                int currNum = numbers[j];
+
+                while (j > 0 && currNum < numbers[j - 1])
+                {
+                    numbers[j] = numbers[j - 1];
+                    j--;
+                }
+
+                numbers[j] = currNum;
+
+            }
+
+            return numbers;
+        }
+
+
+    }
+}
